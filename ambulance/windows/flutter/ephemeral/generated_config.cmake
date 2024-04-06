198d7680dff4728bdf1192126fe5956cabb6ca93
manyahegde <hegdemanya2003@gmail.com>
2024-04-06 11:06:13 +0530

    pull

---
+++ ambulance/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter_windows_3.19.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Hackfest2024\\HF24-Ruby\\ambulance" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter_windows_3.19.5-stable\\flutter"
+  "PROJECT_DIR=E:\\Hackfest2024\\HF24-Ruby\\ambulance"
+  "FLUTTER_ROOT=E:\\flutter_windows_3.19.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Hackfest2024\\HF24-Ruby\\ambulance\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Hackfest2024\\HF24-Ruby\\ambulance"
+  "FLUTTER_TARGET=E:\\Hackfest2024\\HF24-Ruby\\ambulance\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Hackfest2024\\HF24-Ruby\\ambulance\\.dart_tool\\package_config.json"
+)
