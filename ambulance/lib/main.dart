e489afdbf5ed7d8c353cec0ed4df1d84e871fa50
manyahegde <hegdemanya2003@gmail.com>
2024-04-06 09:36:33 +0530

    changes

--- ambulance/lib/main.dart
+++ ambulance/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:ambulance/map_route.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
+  registerWebViewWebImplementation();
   runApp(MyApp());
+}
+
+void registerWebViewWebImplementation() {
+  // Set the platform-specific implementation of WebView
+  WebView.platform = SurfaceAndroidWebView(); // Import SurfaceAndroidWebView if necessary
 }
 
 class MyApp extends StatelessWidget {
@@ -15,5 +22,40 @@ class MyApp extends StatelessWidget {
       ),
       home: MapRoute(), // Use MapRoute as the home page
     );
+  }
+}
+
+class MapRoute extends StatelessWidget {
+  const MapRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //title: Text('Optimal Route'),
+      ),
+      body: FutureBuilder<String>(
+        future: loadHTMLAsset(), // Load the HTML file from assets
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return WebView(
+              initialUrl: Uri.dataFromString(
+                snapshot.data!,
+                mimeType: 'text/html',
+              ).toString(),
+              javascriptMode: JavascriptMode.unrestricted,
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error loading HTML'));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+
+  Future<String> loadHTMLAsset() async {
+    return await rootBundle.loadString('assets/html/optimal_path.html');
   }
 }
