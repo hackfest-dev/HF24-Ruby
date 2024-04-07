264e88cbf4e74524605cf5e22b1c1f9f31182ca0
ApoorvaNayak03 <116479262+ApoorvaNayak03@users.noreply.github.com>
2024-04-07 06:32:56 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById('callButton').addEventListener('click', function() {
+    var phoneNumber = '9591802586'; // Replace this with the desired phone number
+    window.location.href = 'tel:' + phoneNumber;
+});
